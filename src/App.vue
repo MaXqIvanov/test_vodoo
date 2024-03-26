b40dfac32af1f43b645d857a3fe98880d40e064f
max.i <max.i@konomic.com>
2024-03-26 17:40:30 +0300

    added link for github

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
 	<section class="container py-4">
+		<a
+			class="link"
+			target="_blank"
+			href="https://github.com/MaXqIvanov/test_vodoo"
+		>
+			github
+		</a>
 		<RouterView />
 	</section>
 </template>
@@ -9,5 +16,11 @@
 <style scoped lang="scss">
 	.container {
 		min-height: 100vh;
+
+		.link {
+			position: absolute;
+			top: 10px;
+			right: 10px;
+		}
 	}
 </style>
